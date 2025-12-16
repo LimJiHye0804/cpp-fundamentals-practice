708f698bc6cdc0f1288217d5f5944e4c38ae45e6
Jihye080498 <jh080481@pinklab.art>
2025-12-16 14:42:51 +0900

    feat(week1): namespace

--- cpp_week1/Vec2.cpp
+++ cpp_week1/Vec2.cpp
@@ -1,29 +1,31 @@
 #include "Vec2.hpp"
 #include <cmath>
 
-Vec2::Vec2() : x(0.0), y(0.0) {}
+namespace math{
+    Vec2::Vec2() : x(0.0), y(0.0) {}
 
-Vec2::Vec2(double x, double y) : x(x), y(y) {}
+    Vec2::Vec2(double x, double y) : x(x), y(y) {}
 
-double Vec2::length() const {
-    return std::sqrt(x*x + y*y);
-}
+    double Vec2::length() const {
+        return std::sqrt(x*x + y*y);
+    }
 
-Vec2 Vec2::add(const Vec2& other) const {
-    return Vec2(x + other.x, y + other.y);
-}
+    Vec2 Vec2::add(const Vec2& other) const {
+        return Vec2(x + other.x, y + other.y);
+    }
 
-Vec2 Vec2::scale(double s) const {
-    return Vec2(x * s, y * s);
-}
+    Vec2 Vec2::scale(double s) const {
+        return Vec2(x * s, y * s);
+    }
 
-Vec2 Vec2::add_inplace(const Vec2& other) {
-    x += other.x;
-    y += other.y;
-}
+    Vec2 Vec2::add_inplace(const Vec2& other) {
+        x += other.x;
+        y += other.y;
+    }
 
-Vec2& Vec2::add_inplace2(const Vec2& other) {
-    x += other.x;
-    y += other.y;
-    return *this;
+    Vec2& Vec2::add_inplace2(const Vec2& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
 }
--- cpp_week1/Vec2.hpp
+++ cpp_week1/Vec2.hpp
@@ -1,21 +1,22 @@
 #ifndef VEC2_HPP
 #define VEC2_HPP
 
-class Vec2 {
-    public:
-    double x;
-    double y;
+namespace math{
+    class Vec2 {
+        public:
+        double x;
+        double y;
 
-    Vec2();
-    Vec2(double x, double y);
+        Vec2();
+        Vec2(double x, double y);
 
-    double length() const;
-    Vec2 add(const Vec2& other) const;
-    Vec2 scale(double s) const;
+        double length() const;
+        Vec2 add(const Vec2& other) const;
+        Vec2 scale(double s) const;
 
-    // In-place addition
-    Vec2 add_inplace(const Vec2& other);
-    Vec2& add_inplace2(const Vec2& other);
-};
-
+        // In-place addition
+        Vec2 add_inplace(const Vec2& other);
+        Vec2& add_inplace2(const Vec2& other);
+    };
+}
 #endif
